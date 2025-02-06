ada6c5d7028a34b003731cdc3043471078768508
A-freire <bexas94@gmail.com>
2025-02-06 11:41:13 +0100

    qrcode gen

--- iKuKu/iKuKu/QRCodeGen.swift
+++ iKuKu/iKuKu/QRCodeGen.swift
@@ -6,13 +6,43 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
-struct QRCodeGen: View {
+struct QRCodeGeneratorView: View {
+    @State var text: String = "https://apple.com"
+    @State private var qrImage: UIImage?
+
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let image = qrImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .interpolation(.none)
+                    .scaledToFit()
+                    .frame(width: 500, height: 500)
+                    .padding()
+            } else {
+                Text("QR Code non valide")
+                    .foregroundColor(.red)
+            }
+        }
+        .onAppear {
+            generateQRCode(from: text)
+        }
+    }
+
+    // Génère un QR Code à partir d'une chaîne de caractères
+    func generateQRCode(from string: String) {
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                qrImage = UIImage(cgImage: cgimg)
+            }
+        }
     }
 }
 
-#Preview {
-    QRCodeGen()
-}
